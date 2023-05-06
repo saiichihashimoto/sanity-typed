b046c878cd5f5a08f3672a0c0e5b3ca7eb6744d2
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2023-05-06 03:28:55 -0500

    fix(export): export sharedFields as a value, not a type

--- packages/schema-builder/src/index.ts
+++ packages/schema-builder/src/index.ts
@@ -1,6 +1,6 @@
 export type { SanityBlock } from "./block";
 export type { SanityDocument, ParsedSanityDocument } from "./document";
-export type { sharedFields } from "./field";
+export { sharedFields } from "./field";
 export type { SanityFile } from "./file";
 export type { SanityImage } from "./image";
 export type { SanityReference } from "./reference";
