f9a8ca28f56ee9924c01fd071f62ef171d6d6731
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2023-05-08 23:44:20 -0500

    copying in a bunch of @sanity/types tests

--- packages/test-utils/src/index.ts
+++ packages/test-utils/src/index.ts
@@ -1,5 +1,21 @@
 // TODO Release expect-type as it's own package
 
+type ToBeAssignableTo<Expected, Received, Inverted extends boolean> = [
+  Expected
+] extends [Received]
+  ? Inverted extends false
+    ? any
+    : {
+        expected: { not: Expected };
+        received: Received;
+      }
+  : Inverted extends true
+  ? any
+  : {
+      expected: Expected;
+      received: Received;
+    };
+
 // https://twitter.com/mattpocockuk/status/1646452585006604291
 type ToStrictEqual<Expected, Received, Inverted extends boolean> = (<
   T
@@ -22,17 +38,40 @@ const inverted: unique symbol = Symbol("Inverted Brand");
 
 type TypeMatchers<Expected, Inverted extends boolean = false> = {
   [inverted]: Inverted;
+  /** Inverse next matcher. If you know how to test something, .not lets you test its opposite. */
   not: TypeMatchers<Expected, Inverted extends true ? false : true>;
+  /**
+   * Checks if Expected is assignable to Received.
+   *
+   * @example
+   * ```typescript
+   * // Equivalent Checks:
+   * expectType<typeof a>().toBeAssignableTo<B>();
+   *
+   * const something: B = a;
+   * ```
+   */
+  toBeAssignableTo: <
+    Received extends ToBeAssignableTo<Expected, Received, Inverted>
+  >() => void;
+  /**
+   * Checks if Expected and Received are exactly the same type.
+   *
+   * @link https://twitter.com/mattpocockuk/status/1646452585006604291
+   */
   toStrictEqual: <
     Received extends ToStrictEqual<Expected, Received, Inverted>
   >() => void;
 };
 
 export const expectType = <Expected>() => {
-  const val = {
+  const valWithoutNot: Omit<TypeMatchers<Expected>, "not"> = {
     [inverted]: undefined as unknown as false,
+    toBeAssignableTo: () => {},
     toStrictEqual: () => {},
-  } as TypeMatchers<Expected>;
+  };
+
+  const val = valWithoutNot as TypeMatchers<Expected>;
 
   // eslint-disable-next-line fp/no-mutation -- recursion requires mutation
   val.not = val as unknown as typeof val.not;
