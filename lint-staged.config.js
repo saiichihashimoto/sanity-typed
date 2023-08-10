aa51e2c2a50d42cd6012acc88b9ed27a10ce4494
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2023-08-09 22:30:04 -0400

    stash/unstash my own changes when lint-staged runs
    then I can git add . properly

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -4,6 +4,7 @@ const eslintCmd = `cross-env TIMING=1 eslint --quiet --ext .js,.jsx,.ts,.tsx --f
 const prettierCmd = `prettier --ignore-unknown --write`;
 
 // TODO globally run commands should `git add .` BEFORE command brings back stash https://github.com/okonet/lint-staged/issues/1253
+// Until that issue is resolved, we'll always --no-stash and just stash/unstash and add everything on our own
 
 /**
  * @type {import('lint-staged').Config}
@@ -12,24 +13,16 @@ const config = {
   "*.{gif,jpeg,jpg,png,svg}": ["imagemin-lint-staged"],
   "*.{js,jsx,ts,tsx}": [eslintCmd],
   "*": [prettierCmd],
-  "{.eslint*,package.json}": () => [`${eslintCmd} .`, "git add ."],
-  "{.prettier*,package.json}": () => [`${prettierCmd} .`, "git add ."],
-  "{package-lock.json,package.json}": () => [
-    "npm install",
-    "git add package-lock.json",
-  ],
-  "package.json": () => [
-    "manypkg fix",
-    "manypkg check",
-    "git add **/package.json",
-  ],
+  "{.eslint*,package.json}": () => [`${eslintCmd} .`],
+  "{.prettier*,package.json}": () => [`${prettierCmd} .`],
+  "{package-lock.json,package.json}": () => ["npm install"],
+  "package.json": () => ["manypkg fix", "manypkg check"],
   "{_README.md,README.md}": (filenames) =>
     filenames.flatMap((filename) => [
       `bundle exec markdown_helper include ${path.resolve(
         path.dirname(filename),
         "_README.md"
       )} ${path.resolve(path.dirname(filename), "README.md")}`,
-      `git add ${path.resolve(path.dirname(filename), "README.md")}`,
     ]),
   "{.env*,.gitattributes}": (files) =>
     files.map((file) => `sort -o ${file} ${file}`),
