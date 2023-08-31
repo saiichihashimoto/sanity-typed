1e2df8662654458f4f22c65ea3381af2e16e36a7
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2023-08-30 22:52:48 -0500

    fix(groq): precedence for compound expressions
    fixes #219

--- packages/groq/src/compound-expressions.test.ts
+++ packages/groq/src/compound-expressions.test.ts
@@ -25,26 +25,4 @@ describe("compound expressions", () => {
     expect(result).toBe(10);
     expectType<ExecuteQuery<typeof query>>().toStrictEqual<10>();
   });
-
-  it("(((10)))", async () => {
-    const query = "(((10)))";
-    const tree = parse(query);
-    const result = await (await evaluate(tree)).get();
-
-    const desiredTree = {
-      base: {
-        base: { base: { type: "Value", value: 10 }, type: "Group" },
-        type: "Group",
-      },
-      type: "Group",
-    } as const;
-
-    expect(tree).toStrictEqual(desiredTree);
-    expectType<ReadonlyDeep<Parse<typeof query>>>().toStrictEqual<
-      typeof desiredTree
-    >();
-
-    expect(result).toBe(10);
-    expectType<ExecuteQuery<typeof query>>().toStrictEqual<10>();
-  });
 });
