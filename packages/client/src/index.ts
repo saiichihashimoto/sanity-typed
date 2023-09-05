3145b18287249ef8881fe7269339f1f3cf8b62a3
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2023-09-05 01:10:20 -0500

    feat(client): a few more of client's methods #286

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -2,40 +2,103 @@ import { createClient as createClientNative } from "@sanity/client";
 import type {
   ClientConfig,
   FilteredResponseQueryOptions,
+  InitializedClientConfig as InitializedClientConfigNative,
   QueryParams,
+  RawQueryResponse as RawQueryResponseNative,
   SanityClient as SanityClientNative,
   UnfilteredResponseQueryOptions,
 } from "@sanity/client";
-import type { Merge } from "type-fest";
+import type { Merge, WritableDeep } from "type-fest";
 
 import type { ExecuteQuery, RootScope } from "@sanity-typed/groq";
 import type { SanityDocument } from "@sanity-typed/types";
 
+export type InitializedClientConfig<TClientConfig extends ClientConfig> = Merge<
+  InitializedClientConfigNative,
+  TClientConfig
+>;
+
+export type RawQueryResponse<Result, Query extends string = string> = Merge<
+  RawQueryResponseNative<Result>,
+  {
+    query: Query;
+  }
+>;
+
+type MaybeRawQueryResponse<
+  Result,
+  Query extends string,
+  Options extends
+    | FilteredResponseQueryOptions
+    | UnfilteredResponseQueryOptions
+    | undefined
+> = Options extends UnfilteredResponseQueryOptions
+  ? RawQueryResponse<Result, Query>
+  : Result;
+
+type GetDocuments<
+  Ids extends string[],
+  TDocument extends Omit<SanityDocument, "_type">
+> = { [index in keyof Ids]: (TDocument & { _id: Ids[index] }) | null };
+
 // eslint-disable-next-line @typescript-eslint/consistent-type-definitions -- recursive type
 export interface SanityClient<
-  Client extends ClientConfig,
-  Dataset extends any[]
-> extends Omit<SanityClientNative, "clone" | "fetch" | "withConfig"> {
-  clone: () => SanityClient<Client, Dataset>;
-  fetch: <Query extends string, const Q = QueryParams>(
+  TClientConfig extends ClientConfig,
+  TDocument extends Omit<SanityDocument, "_type">
+> extends Omit<
+    SanityClientNative,
+    "clone" | "config" | "fetch" | "getDocument" | "getDocuments" | "withConfig"
+  > {
+  clone: () => SanityClient<TClientConfig, TDocument>;
+  config: <
+    const NewConfig extends Partial<ClientConfig> | undefined = undefined
+  >(
+    newConfig?: NewConfig
+  ) => NewConfig extends undefined
+    ? InitializedClientConfig<WritableDeep<TClientConfig>>
+    : SanityClient<Merge<TClientConfig, NewConfig>, TDocument>;
+  fetch: <
+    const Query extends string,
+    const Q = QueryParams,
+    const Options extends
+      | FilteredResponseQueryOptions
+      | UnfilteredResponseQueryOptions
+      | undefined = undefined
+  >(
     query: Query,
     params?: Q,
-    options?: FilteredResponseQueryOptions | UnfilteredResponseQueryOptions
+    options?: Options
   ) => Promise<
-    ExecuteQuery<
+    MaybeRawQueryResponse<
+      ExecuteQuery<
+        Query,
+        RootScope<{
+          client: WritableDeep<TClientConfig>;
+          dataset: (TDocument &
+            (TClientConfig extends { perspective: "previewDrafts" }
+              ? { _originalId: string }
+              : unknown))[];
+          delta: { after: null; before: null };
+          parameters: NonNullable<Q>;
+        }>
+      >,
       Query,
-      RootScope<{
-        client: Client;
-        dataset: Dataset;
-        delta: { after: null; before: null };
-        parameters: NonNullable<Q>;
-      }>
+      Options
     >
   >;
+  getDocument: <const Id extends string>(
+    id: Id,
+    options?: Parameters<SanityClientNative["getDocument"]>[1]
+  ) => Promise<(TDocument & { _id: Id }) | undefined>;
+  getDocuments: <const Ids extends readonly string[]>(
+    ids: Ids,
+    options?: Parameters<SanityClientNative["getDocuments"]>[1]
+  ) => Promise<GetDocuments<WritableDeep<Ids>, TDocument>>;
   withConfig: <const NewConfig extends Partial<ClientConfig>>(
     newConfig?: NewConfig
-  ) => SanityClient<Merge<Client, NewConfig>, Dataset>;
+  ) => SanityClient<Merge<TClientConfig, NewConfig>, TDocument>;
 }
+
 /**
  * Unfortunately, this has to have a very weird function signature due to this typescript issue:
  * https://github.com/microsoft/TypeScript/issues/10571
@@ -45,5 +108,5 @@ export const createClient =
   <const Config extends ClientConfig>(config: Config) =>
     createClientNative(config) as unknown as SanityClient<
       Config,
-      Extract<Values[keyof Values], Omit<SanityDocument, "_type">>[]
+      Extract<Values[keyof Values], Omit<SanityDocument, "_type">>
     >;
