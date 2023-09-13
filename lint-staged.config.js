cbc37ff7e81dd693ae416b9e3811d82ce768f90e
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2023-09-13 00:12:21 -0500

    cache prettier, eslint, and jest

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
-const eslintCmd = `cross-env TIMING=1 eslint --quiet --ext .js,.jsx,.ts,.tsx --fix`;
-const prettierCmd = `prettier --ignore-unknown --write`;
+const eslintCmd = `cross-env TIMING=1 eslint --quiet --ext .js,.jsx,.ts,.tsx --fix --cache --cache-strategy content`;
+const prettierCmd = `prettier --ignore-unknown --write --cache`;
 
 // TODO globally run commands should `git add .` BEFORE command brings back stash https://github.com/okonet/lint-staged/issues/1253
 // Until that issue is resolved, we'll always --no-stash and just stash/unstash and add everything on our own
