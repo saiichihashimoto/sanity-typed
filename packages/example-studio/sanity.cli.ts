1fddb37926cd130b436c16ca4c72d1d3cc31c116
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2023-11-02 16:25:41 -0500

    example-studio build/dev for sanity & tsup

---
+++ packages/example-studio/sanity.cli.ts
@@ -0,0 +1,23 @@
+/**
+ * DONT COPY THIS FILE
+ *
+ * This is because we're using path aliases in the monorepo's tsconfig and is
+ * not required for @sanity-typed/*.
+ * https://github.com/sanity-io/sanity/issues/5115
+ */
+import { dirname, resolve } from "path";
+import { defineCliConfig } from "sanity/cli";
+import { fileURLToPath } from "url";
+
+export default defineCliConfig({
+  vite: {
+    resolve: {
+      alias: {
+        "@sanity-typed/types": resolve(
+          dirname(fileURLToPath(import.meta.url)),
+          "../types/src"
+        ),
+      },
+    },
+  },
+});
