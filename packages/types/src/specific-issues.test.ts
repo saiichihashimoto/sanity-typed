4a364382e9faca802f38256e79b54b0db95f49a5
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2024-01-21 13:22:43 -0600

    test for #622

--- packages/types/src/specific-issues.test.ts
+++ packages/types/src/specific-issues.test.ts
@@ -4,7 +4,12 @@ import { expectType } from "@saiichihashimoto/test-utils";
 import type { decorator } from "@portabletext-typed/types";
 
 import { defineArrayMember, defineConfig, defineField, defineType } from ".";
-import type { ImageValue, InferSchemaValues, ReferenceValue } from ".";
+import type {
+  ImageValue,
+  InferSchemaValues,
+  ReferenceValue,
+  referenced,
+} from ".";
 
 describe("specific issues", () => {
   it("#108 object -> array -> object -> object -> array -> object -> image", () => {
@@ -173,7 +178,7 @@ describe("specific issues", () => {
     }>();
   });
 
-  it("#589 object -> array -> block -> object -> field", async () => {
+  it("#589 object -> array -> block -> object -> field", () => {
     const config = defineConfig({
       dataset: "dataset",
       projectId: "projectId",
@@ -248,6 +253,120 @@ describe("specific issues", () => {
           | "h6"
           | "normal";
       }[];
+    }>();
+  });
+
+  it("#622 ???", () => {
+    const config = defineConfig({
+      dataset: "dataset",
+      projectId: "projectId",
+      schema: {
+        types: [
+          defineType({
+            name: "foo",
+            type: "document",
+            fields: [
+              defineField({
+                name: "content",
+                type: "array",
+                of: [
+                  defineArrayMember({ type: "image" }),
+                  defineArrayMember({
+                    type: "block",
+                    of: [defineArrayMember({ type: "file" })],
+                    styles: [
+                      { title: "Normal", value: "normal" as const },
+                      { title: "Foo", value: "foo" as const },
+                    ],
+                    lists: [
+                      { title: "Bullet", value: "bullet" as const },
+                      { title: "Bar", value: "bar" as const },
+                    ],
+                    marks: {
+                      decorators: [
+                        { title: "Strong", value: "strong" as const },
+                        { title: "Baz", value: "baz" as const },
+                      ],
+                      annotations: [
+                        defineArrayMember({
+                          name: "link",
+                          type: "object",
+                          fields: [
+                            defineField({
+                              name: "href",
+                              type: "string",
+                              validation: (Rule) => Rule.required(),
+                            }),
+                          ],
+                        }),
+                        defineArrayMember({
+                          name: "qux",
+                          type: "object",
+                          fields: [
+                            defineField({
+                              name: "value",
+                              type: "string",
+                              validation: (Rule) => Rule.required(),
+                            }),
+                          ],
+                        }),
+                      ],
+                    },
+                  }),
+                ],
+              }),
+            ],
+          }),
+        ],
+      },
+    });
+
+    expectType<InferSchemaValues<typeof config>["foo"]>().toEqual<{
+      _createdAt: string;
+      _id: string;
+      _rev: string;
+      _type: "foo";
+      _updatedAt: string;
+      content?: (
+        | {
+            _key: string;
+            _type: "block";
+            children: (
+              | {
+                  _key: string;
+                  _type: "file";
+                  asset: {
+                    _ref: string;
+                    _type: "reference";
+                    [referenced]: "sanity.fileAsset";
+                  };
+                }
+              | {
+                  _key: string;
+                  _type: "span";
+                  [decorator]: "baz" | "strong";
+                  marks: string[];
+                  text: string;
+                }
+            )[];
+            level?: number;
+            listItem?: "bar" | "bullet";
+            markDefs: (
+              | { _key: string; _type: "link"; href: string }
+              | { _key: string; _type: "qux"; value: string }
+            )[];
+            style: "foo" | "normal";
+          }
+        | {
+            _key: string;
+            _type: "image";
+            asset: {
+              _ref: string;
+              _type: "reference";
+              [referenced]: "sanity.imageAsset";
+            };
+          }
+      )[];
     }>();
   });
 });
